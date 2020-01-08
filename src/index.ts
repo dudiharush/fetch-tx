e2f97d2cf8f09f51104cd1a35d5de5448ba76358
David Harush <davidharush@gmail.com>
2020-01-08 12:10:32 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 export function getFetchTx() {
-  let controller = new AbortController();
-  let signal = controller.signal;
-    async function fetchTx<Response> (url: string, init?: RequestInit) {
-    let res, resJson;
+  let controller = new AbortController()
+  let signal = controller.signal
+  async function fetchTx<Response>(url: string, init?: RequestInit) {
+    let res, resJson
+
     try {
-      res = await fetch(url, {...init, signal });
-      resJson = await res.json();
+      res = await fetch(url, { ...init, signal })
+      resJson = await res.json()
     } catch (err) {
-      if (err.name === "AbortError") console.log("request aborted");
+      if (err.name === 'AbortError') console.log('request aborted')
     }
-    return resJson as Response;
-  };
+    return resJson as Response
+  }
   fetchTx.abort = () => {
-    controller.abort();
-    controller = new AbortController();
-    signal = controller.signal;
-  };
-  return fetchTx;
+    controller.abort()
+    controller = new AbortController()
+    signal = controller.signal
+  }
+  return fetchTx
 }
